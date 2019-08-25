664794c9f157e90127af7cc72fca3c14b932c8bd
jackofdiamond5 <jackofdiamond596@gmail.com>
2019-08-25 13:24:54 +0300

    refactor: add ingredients to recipe details

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,24 @@
 import { Recipe } from './recipe.model';
 import { Injectable, EventEmitter } from '@angular/core';
 import { RecipesComponent } from './recipes.component';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     public recipeSelected = new EventEmitter<Recipe>();
 
     private _recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is a test recipe', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-        new Recipe('Another test recipe', 'This is a test recipe', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+        new Recipe(
+            'A test recipe',
+            'This is a test recipe',
+            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+            [new Ingredient('tomatoe', 1), new Ingredient('onion', 2)]
+        ),
+        new Recipe(
+            'Another test recipe',
+            'This is a test recipe',
+            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+            [new Ingredient('potatoe', 3), new Ingredient('olive', 6)]
+        )
     ];
 
     public get recipes() {
